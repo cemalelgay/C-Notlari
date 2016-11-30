81abe94efc2c14ee7ee248b04437ca42160dc8bd
cemal.elgay <celgay@hotmail.com>
2016-11-30 09:53:48 +0200

    Update

---
+++ Tekrar/Tekrar/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tekrar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Dört işlem.
+
+            //bool devam = true;
+
+            //while (devam)
+            //{
+            //    Console.Write("Birinci Sayi: ");
+            //    double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+            //    Console.Write("İkinci Sayi: ");
+            //    double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            //    Console.Write("İslem Sec: ");
+            //    char islem = Convert.ToChar(Console.ReadLine());
+
+            //    double sonuc;
+
+            //    switch (islem)
+            //    {
+            //        case '+':
+            //            sonuc = sayi1 + sayi2;
+            //            break;
+            //        case '-':
+            //            sonuc = sayi1 - sayi2;
+            //            break;
+            //        case '*':
+            //            sonuc = sayi1 * sayi2;
+            //            break;
+            //        case '/':
+            //            sonuc = sayi1 / sayi2;
+            //            break;
+
+            //        default:
+            //            Console.WriteLine("Böyle bir islem tipi yok!");
+            //            sonuc = 0;
+            //            break;
+
+            //    }
+            //    Console.WriteLine("Sonuc: " + sonuc);
+
+            //    Console.WriteLine("Devam edelim mi? (evet)");
+            //    string cevap = Console.ReadLine();
+
+            //    if (cevap == "evet")
+            //    {
+            //        devam = true;
+            //    }
+            //    else
+            //    {
+            //        devam = false;
+            //    }
+
+            //}
+            //Console.WriteLine("Görüşmek Üzere");
+            //Console.ReadLine();
+
+            #endregion
+
+
+            #region
+
+
+
+
+
+
+            #endregion
+
+
+
+
+
+        }
+    }
+}
