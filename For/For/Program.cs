dcb077a56414b4346282cf0f08bdeac9dd23c4e5
cemal.elgay <celgay@hotmail.com>
2016-11-30 12:07:24 +0200

    Update

--- For/For/Program.cs
+++ For/For/Program.cs
@@ -87,8 +87,39 @@ namespace For
             //Console.ReadKey();
             #endregion
 
-            #region 
+            #region İçİçeFor
 
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 0; j < 5; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //Console.ReadKey();
+            #endregion
+
+            #region Harfler
+            //for (char c = 'a'; c <= 'z'; c++)
+            //{
+            //    Console.Write(c + " ");
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region ÇarpımTablosu
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    for (int j = 1; j <= 10; j++)
+            //    {
+            //        Console.WriteLine("{0} x {1} \t = {2}", i, j, i * j);
+            //    }
+            //    Console.WriteLine("***********");
+            //}
+            //Console.ReadKey();
             #endregion
 
         }
