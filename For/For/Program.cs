2ac664db121baa41e66dbf4e3a1f5b4af6e36319
cemal.elgay <celgay@hotmail.com>
2016-11-30 11:50:04 +0200

    Update

--- For/For/Program.cs
+++ For/For/Program.cs
@@ -43,7 +43,53 @@ namespace For
 
             #endregion
 
+            #region BirdenFazlaKontrol
+            //bool durum = true;
+            //for (int i = 0; durum == true && i < 100; i++) //durum==true
+            //{
+            //    if (i * 2 > 20)
+            //    {
+            //        durum = false;
+            //        //break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("{0}İken devam ediyorum", i);
+            //    }
+            //}
+            //Console.ReadKey();
+            #endregion
 
+            #region BreakCountinue
+            //for (int i = 0; i < 100; i += 5)
+            //{
+            //    if (i * 2 < 20)
+            //    {
+            //        //break; İşlemden direk çıkartır. // döngüyü sonlandırır.
+            //        continue; // bir sonrakine geçirir. // bir sonraki değere gönderir - geçirir.
+            //    }
+            //    Console.WriteLine("{0} iken devam ediyorum", i);
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region [1,n]Aralığindaki sayilarin toplami
+            //int n = Convert.ToInt32(Console.ReadLine());
+            ////int n = Convert.ToInt32("5");
+
+            //int toplam = 0;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    toplam += i;
+            //    Console.WriteLine("i : {0} \t Toplam: {1}", i, toplam);
+            //}
+            //Console.WriteLine("Genel Toplam : " + toplam);
+            //Console.ReadKey();
+            #endregion
+
+            #region 
+
+            #endregion
 
         }
     }
