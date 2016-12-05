d2e31981b5da3a5991692d27de6e9c080b2f37c2
cemal.elgay <celgay@hotmail.com>
2016-12-05 11:56:12 +0200

    Update

---
+++ Array/Array/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Array TANIM...!!!
+
+            //TANIM!!!
+
+            //int[] array = new int[3];
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = i;
+            //}
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Sınıf Mevcudu Yapımı
+
+            //Sınıf Mevcudu Yapımı...
+
+            //Console.Write("Sınıf Mevcudu ? : ");
+            //int mevcut = Convert.ToInt32(Console.ReadLine());
+
+            //string[] ogrenciler = new string[mevcut];
+
+            //for (int i = 0; i < ogrenciler.Length; i++)
+            //{
+            //    Console.Write("{0}.Ögrencini İsmi: ", i);
+            //    ogrenciler[i] = Console.ReadLine();
+            //}
+
+            //Console.Clear();
+
+            //Console.WriteLine("Öğrenciler: \n");
+
+            //foreach (var İtem in ogrenciler)
+            //{
+            //    Console.WriteLine(İtem);
+            //}
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region  [-100 , 100 arası sayı üretip pozitif negatif]
+
+            // [-100 , 100 arası sayı üretip pozitif negatif]
+
+            //Random random = new Random();
+            //int pozitif = 0, negatif = 0, sifir = 0;
+
+            //Console.Write("Bir sayı girin: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int[] sayilar = new int[n];
+
+            //Console.Clear();
+
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    sayilar[i] = random.Next(-100, 101);
+            //}
+            //foreach (var sayi in sayilar)
+            //{
+            //    Console.WriteLine(sayi);
+            //    if (sayi > 0)
+            //    {
+            //        pozitif++;
+            //    }
+            //    else if (sayi < 0)
+            //    {
+            //        negatif++;
+            //    }
+            //    else
+            //    {
+            //        sifir++;
+            //    }
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine(" {0} Tane Pozitif var.", pozitif);
+            //Console.WriteLine(" {0} Tane Negatif var.", negatif);
+            //Console.WriteLine(" {0} Tane Sifir var.", sifir);
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Değer Atama
+
+            // değer atama
+
+            //string[] günler = new string[]("Pazartesi", "Sali", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar");
+
+            //foreach (var gun in günler)
+            //{
+            //    Console.WriteLine(gun);
+            //}
+            #endregion
+
+            #region Günler
+
+            //string[] gunler = new string[]("Pazartesi", "Sali", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar");
+
+            //foreach (var gun in gunler)
+            //{
+            //    Console.WriteLine(gun);
+            //}
+
+            #endregion
+
+
+        }
+    }
+}
