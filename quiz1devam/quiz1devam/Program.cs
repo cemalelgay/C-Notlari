23fef242ab871d0aea7f94ec657d0e67d7d141ee
cemal.elgay <celgay@hotmail.com>
2016-12-05 10:41:08 +0200

    Update

---
+++ quiz1devam/quiz1devam/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz1devam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Paralel
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (i == 0 && j >= 2)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    if (i == 1 && (j == 1 || j == 4))
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    if (i == 2 && j <= 3)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+                Console.ReadKey();
+            }           
+        }
+    }
+}
