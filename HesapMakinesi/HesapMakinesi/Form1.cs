118bbdf98a360e8e88596fec72a22e51ab1a0a77
cemal.elgay <celgay@hotmail.com>
2016-12-07 11:38:52 +0200

    Update

---
+++ HesapMakinesi/HesapMakinesi/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button remzi = sender as Button;
+            // Button ramiz = (Button)sender;
+
+            MessageBox.Show(remzi.Text + " e bastın.");
+        }
+    }
+}
