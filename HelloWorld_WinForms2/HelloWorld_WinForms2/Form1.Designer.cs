fa97b4b805d1111e2e49552ca07b2a97ed5ee7e9
cemal.elgay <celgay@hotmail.com>
2016-12-07 11:25:31 +0200

    Update

---
+++ HelloWorld_WinForms2/HelloWorld_WinForms2/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace HelloWorld_WinForms2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.KaydetBtn = new System.Windows.Forms.Button();
+            this.AdTxtB = new System.Windows.Forms.TextBox();
+            this.SifreTxtB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // KaydetBtn
+            // 
+            this.KaydetBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("KaydetBtn.BackgroundImage")));
+            this.KaydetBtn.Location = new System.Drawing.Point(104, 134);
+            this.KaydetBtn.Name = "KaydetBtn";
+            this.KaydetBtn.Size = new System.Drawing.Size(34, 37);
+            this.KaydetBtn.TabIndex = 0;
+            this.KaydetBtn.UseVisualStyleBackColor = true;
+            this.KaydetBtn.Click += new System.EventHandler(this.KaydetBtn_Click);
+            // 
+            // AdTxtB
+            // 
+            this.AdTxtB.Location = new System.Drawing.Point(71, 42);
+            this.AdTxtB.Name = "AdTxtB";
+            this.AdTxtB.Size = new System.Drawing.Size(100, 20);
+            this.AdTxtB.TabIndex = 1;
+            // 
+            // SifreTxtB
+            // 
+            this.SifreTxtB.Location = new System.Drawing.Point(70, 99);
+            this.SifreTxtB.Name = "SifreTxtB";
+            this.SifreTxtB.Size = new System.Drawing.Size(100, 20);
+            this.SifreTxtB.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ad Soyad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sifre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(284, 183);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SifreTxtB);
+            this.Controls.Add(this.AdTxtB);
+            this.Controls.Add(this.KaydetBtn);
+            this.Name = "Form1";
+            this.Text = "İlk Uygulamam";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button KaydetBtn;
+        private System.Windows.Forms.TextBox AdTxtB;
+        private System.Windows.Forms.TextBox SifreTxtB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HelloWorld_WinForms2/HelloWorld_WinForms2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld_WinForms2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Merhaba İzmir");
+        }
+
+        private void KaydetBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ad: " + AdTxtB.Text + "\nSifre: " + SifreTxtB.Text);
+        }
+    }
+}
