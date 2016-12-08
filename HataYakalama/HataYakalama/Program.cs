af6afd4dd2d21e742c7047e87548dcf3106fa6fd
cemal.elgay <celgay@hotmail.com>
2016-12-08 11:15:16 +0200

    Update

---
+++ HataYakalama/HataYakalama/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HataYakalama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region TANIM...!!!
+
+            //int[] array = new int[4];
+            //Console.WriteLine(array[5]);
+
+            //try
+            //{
+            //    int[] array = new int[4];
+            //    Console.WriteLine(array[5]);
+            //}
+            //catch (IndexOutOfRangeException ex)
+            //{
+            //    //Console.WriteLine(ex.Message);
+            //    Console.WriteLine("Böyle bir dizin yok");
+            //    Console.ReadLine();
+            //}
+            #endregion
+
+
+            #region MyRegion
+
+            //try
+            //{
+            //    int sayi = int.Parse(Console.ReadLine());
+            //}
+            //catch (FormatException ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Finalliy çalıştı.");
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region MyRegion
+
+            //try
+            //{
+            //    int sayi = int.Parse(Console.ReadLine());
+            //}
+            //catch (FormatException ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}
+            //catch (OverflowException ex) //değişken(int) sınırlarını aşarsa 2.catchden dolayı hata veriyor.
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Finalliy Çalıştı.");
+            //}
+            //Console.ReadLine();
+            #endregion
+
+            #region MyRegion
+
+            //try
+            //{
+            //    int sayi = int.Parse(Console.ReadLine());
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //Console.ReadKey();
+
+            #endregion
+
+            #region MyRegion
+
+            //try
+            //{
+            //    throw new IndexOutOfRangeException();
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}
+            //Console.ReadKey();
+
+            #endregion
+
+
+        }
+    }
+}
