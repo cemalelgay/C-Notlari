34cb4e22cce17d90e2130e4bcdcd0e1c966abab9
cemal.elgay <celgay@hotmail.com>
2016-12-12 09:45:55 +0200

    Update

--- SansTopu/SansTopu/Form1.Designer.cs
+++ SansTopu/SansTopu/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.oynaBtn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -35,6 +36,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // oynaBtn
@@ -107,6 +109,10 @@
             this.label6.TabIndex = 6;
             this.label6.Text = "label6";
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -135,6 +141,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- SansTopu/SansTopu/Form1.cs
+++ SansTopu/SansTopu/Form1.cs
@@ -19,32 +19,56 @@ namespace SansTopu
 
         Random rnd = new Random();
         List<int> Sayilar = new List<int>(6);
-        int rasgeleSayi;
-
+        int RasgeleSayi;
+        int tur = 1;
         int sayac = 0;
 
 
         private void oynaBtn_Click(object sender, EventArgs e)
         {
-            while (sayac < 6)
-            {
-                rasgeleSayi = rnd.Next(1, 50);
-                if(!Sayilar.Contains(rasgeleSayi))
-                {
-                    Sayilar.Add(rasgeleSayi);
-                    sayac++;
-                }               
-            }
-            Sayilar.Sort();
+            timer1.Start();
+            tur = 1;
+        }
 
-            label1.Text = Sayilar[0].ToString();
-            label2.Text = Sayilar[1].ToString();
-            label3.Text = Sayilar[2].ToString();
-            label4.Text = Sayilar[3].ToString();
-            label5.Text = Sayilar[4].ToString();
-            label6.Text = Sayilar[5].ToString();
-        
-            sayac = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+
+        {
+            if (tur >= 15)
+            {
+                timer1.Interval = 500;
+            }
+            if (tur % 20 == 0)
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                Sayilar.Clear();
+
+                while (sayac < 6)
+                {
+                    RasgeleSayi = rnd.Next(1, 50);
+                    if (!Sayilar.Contains(RasgeleSayi))
+                    {
+                        Sayilar.Add(RasgeleSayi);
+                        sayac++;
+                    }
+                }
+
+                Sayilar.Sort();
+
+
+
+                label1.Text = Sayilar[0].ToString();
+                label2.Text = Sayilar[1].ToString();
+                label3.Text = Sayilar[2].ToString();
+                label4.Text = Sayilar[3].ToString();
+                label5.Text = Sayilar[4].ToString();
+                label6.Text = Sayilar[5].ToString();
+
+                sayac = 0;
+                tur++;
+            }
         }
     }
 }
