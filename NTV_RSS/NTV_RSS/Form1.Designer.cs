1ffa549558cecf4cee27fbb1d2e4bf1f5b455a9a
cemal.elgay <celgay@hotmail.com>
2017-01-03 12:00:45 +0200

    Updated

---
+++ NTV_RSS/NTV_RSS/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace NTV_RSS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnGetir = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BtnGetir
+            // 
+            this.BtnGetir.Location = new System.Drawing.Point(13, 13);
+            this.BtnGetir.Name = "BtnGetir";
+            this.BtnGetir.Size = new System.Drawing.Size(75, 23);
+            this.BtnGetir.TabIndex = 0;
+            this.BtnGetir.Text = "GETİR";
+            this.BtnGetir.UseVisualStyleBackColor = true;
+            this.BtnGetir.Click += new System.EventHandler(this.BtnGetir_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 64);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(537, 264);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 345);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.BtnGetir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnGetir;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ NTV_RSS/NTV_RSS/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+namespace NTV_RSS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGetir_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            XmlTextReader xmlDoc = new XmlTextReader("http://www.ntv.com.tr/gundem.rss");
+
+            while (xmlDoc.Read())
+            {
+                if (xmlDoc.NodeType == XmlNodeType.Element && xmlDoc.Name == "title")
+                {
+                    listBox1.Items.Add(xmlDoc.ReadString());
+                }
+            }
+            listBox1.Items.RemoveAt(0);
+        }
+    }
+}
