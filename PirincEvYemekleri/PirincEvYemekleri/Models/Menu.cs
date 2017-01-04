747e40b7148361f0c1fac9d946611d96756886e4
cemal.elgay <celgay@hotmail.com>
2017-01-04 13:25:22 +0200

    Updated

---
+++ PirincEvYemekleri/PirincEvYemekleri/Models/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PirincEvYemekleri.Models
+{
+    class Menu
+    {
+        public List<AnaYemek> name { get; set; }
+        public List<Salata> salatalar { get; set; }
+        public List<Tatli> tatlilar { get; set; }
+    }
+}
