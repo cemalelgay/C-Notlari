1da74540da0bb5c1d27c1c366f9450a8231f9b32
cemal.elgay <celgay@hotmail.com>
2017-01-05 12:31:00 +0200

    Updated

--- CemalElgay_KafeSiparisi/CemalElgay_KafeSiparisi/Form1.Designer.cs
+++ CemalElgay_KafeSiparisi/CemalElgay_KafeSiparisi/Form1.Designer.cs
@@ -30,21 +30,22 @@
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ComboBoxIcecek = new System.Windows.Forms.ComboBox();
+            this.ComboBoxYemek = new System.Windows.Forms.ComboBox();
+            this.NumericIcecek = new System.Windows.Forms.NumericUpDown();
+            this.NumericYemek = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.BtnSiparis = new System.Windows.Forms.Button();
             this.BtnOdeme = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.NumericYemek = new System.Windows.Forms.NumericUpDown();
-            this.NumericIcecek = new System.Windows.Forms.NumericUpDown();
-            this.ComboBoxYemek = new System.Windows.Forms.ComboBox();
-            this.ComboBoxIcecek = new System.Windows.Forms.ComboBox();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radiobutton4 = new System.Windows.Forms.RadioButton();
             this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.NumericYemek)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.NumericIcecek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericYemek)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -52,7 +53,7 @@
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.groupBox1.Location = new System.Drawing.Point(13, 27);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(217, 232);
+            this.groupBox1.Size = new System.Drawing.Size(236, 292);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Masalar";
@@ -66,12 +67,60 @@
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.groupBox2.Location = new System.Drawing.Point(13, 308);
+            this.groupBox2.Location = new System.Drawing.Point(13, 334);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(345, 147);
+            this.groupBox2.Size = new System.Drawing.Size(345, 121);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Siparis";
+            // 
+            // ComboBoxIcecek
+            // 
+            this.ComboBoxIcecek.FormattingEnabled = true;
+            this.ComboBoxIcecek.Location = new System.Drawing.Point(69, 85);
+            this.ComboBoxIcecek.Name = "ComboBoxIcecek";
+            this.ComboBoxIcecek.Size = new System.Drawing.Size(209, 24);
+            this.ComboBoxIcecek.TabIndex = 6;
+            // 
+            // ComboBoxYemek
+            // 
+            this.ComboBoxYemek.FormattingEnabled = true;
+            this.ComboBoxYemek.Location = new System.Drawing.Point(69, 42);
+            this.ComboBoxYemek.Name = "ComboBoxYemek";
+            this.ComboBoxYemek.Size = new System.Drawing.Size(209, 24);
+            this.ComboBoxYemek.TabIndex = 5;
+            // 
+            // NumericIcecek
+            // 
+            this.NumericIcecek.Location = new System.Drawing.Point(284, 85);
+            this.NumericIcecek.Name = "NumericIcecek";
+            this.NumericIcecek.Size = new System.Drawing.Size(45, 23);
+            this.NumericIcecek.TabIndex = 3;
+            // 
+            // NumericYemek
+            // 
+            this.NumericYemek.Location = new System.Drawing.Point(284, 40);
+            this.NumericYemek.Name = "NumericYemek";
+            this.NumericYemek.Size = new System.Drawing.Size(45, 23);
+            this.NumericYemek.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "İçecek";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Yemek";
             // 
             // listBox1
             // 
@@ -83,75 +132,31 @@
             // 
             // BtnSiparis
             // 
+            this.BtnSiparis.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnSiparis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.BtnSiparis.Location = new System.Drawing.Point(13, 461);
             this.BtnSiparis.Name = "BtnSiparis";
-            this.BtnSiparis.Size = new System.Drawing.Size(177, 23);
+            this.BtnSiparis.Size = new System.Drawing.Size(177, 30);
             this.BtnSiparis.TabIndex = 3;
             this.BtnSiparis.Text = "SİPARİŞ VER";
-            this.BtnSiparis.UseVisualStyleBackColor = true;
+            this.BtnSiparis.UseVisualStyleBackColor = false;
             // 
             // BtnOdeme
             // 
-            this.BtnOdeme.Location = new System.Drawing.Point(345, 221);
+            this.BtnOdeme.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnOdeme.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnOdeme.Location = new System.Drawing.Point(345, 245);
             this.BtnOdeme.Name = "BtnOdeme";
-            this.BtnOdeme.Size = new System.Drawing.Size(75, 23);
+            this.BtnOdeme.Size = new System.Drawing.Size(75, 28);
             this.BtnOdeme.TabIndex = 4;
             this.BtnOdeme.Text = "ÖDEME";
-            this.BtnOdeme.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 70);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Yemek";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 113);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "İçecek";
-            // 
-            // NumericYemek
-            // 
-            this.NumericYemek.Location = new System.Drawing.Point(284, 68);
-            this.NumericYemek.Name = "NumericYemek";
-            this.NumericYemek.Size = new System.Drawing.Size(45, 23);
-            this.NumericYemek.TabIndex = 2;
-            // 
-            // NumericIcecek
-            // 
-            this.NumericIcecek.Location = new System.Drawing.Point(284, 113);
-            this.NumericIcecek.Name = "NumericIcecek";
-            this.NumericIcecek.Size = new System.Drawing.Size(45, 23);
-            this.NumericIcecek.TabIndex = 3;
-            // 
-            // ComboBoxYemek
-            // 
-            this.ComboBoxYemek.FormattingEnabled = true;
-            this.ComboBoxYemek.Location = new System.Drawing.Point(69, 70);
-            this.ComboBoxYemek.Name = "ComboBoxYemek";
-            this.ComboBoxYemek.Size = new System.Drawing.Size(209, 24);
-            this.ComboBoxYemek.TabIndex = 5;
-            // 
-            // ComboBoxIcecek
-            // 
-            this.ComboBoxIcecek.FormattingEnabled = true;
-            this.ComboBoxIcecek.Location = new System.Drawing.Point(69, 113);
-            this.ComboBoxIcecek.Name = "ComboBoxIcecek";
-            this.ComboBoxIcecek.Size = new System.Drawing.Size(209, 24);
-            this.ComboBoxIcecek.TabIndex = 6;
+            this.BtnOdeme.UseVisualStyleBackColor = false;
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton1.Location = new System.Drawing.Point(274, 194);
+            this.radioButton1.Location = new System.Drawing.Point(274, 218);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(65, 21);
             this.radioButton1.TabIndex = 5;
@@ -163,7 +168,7 @@
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton2.Location = new System.Drawing.Point(345, 194);
+            this.radioButton2.Location = new System.Drawing.Point(411, 191);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(60, 21);
             this.radioButton2.TabIndex = 6;
@@ -175,7 +180,7 @@
             // 
             this.radioButton3.AutoSize = true;
             this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.radioButton3.Location = new System.Drawing.Point(411, 194);
+            this.radioButton3.Location = new System.Drawing.Point(411, 218);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(72, 21);
             this.radioButton3.TabIndex = 7;
@@ -183,11 +188,24 @@
             this.radioButton3.Text = "Pound";
             this.radioButton3.UseVisualStyleBackColor = true;
             // 
+            // radiobutton4
+            // 
+            this.radiobutton4.AutoSize = true;
+            this.radiobutton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radiobutton4.Location = new System.Drawing.Point(273, 195);
+            this.radiobutton4.Name = "radiobutton4";
+            this.radiobutton4.Size = new System.Drawing.Size(45, 21);
+            this.radiobutton4.TabIndex = 8;
+            this.radiobutton4.TabStop = true;
+            this.radiobutton4.Text = "TL";
+            this.radiobutton4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(538, 503);
+            this.ClientSize = new System.Drawing.Size(538, 533);
+            this.Controls.Add(this.radiobutton4);
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
@@ -201,8 +219,8 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.NumericYemek)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.NumericIcecek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericYemek)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -224,6 +242,7 @@
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radiobutton4;
     }
 }
 
--- CemalElgay_KafeSiparisi/CemalElgay_KafeSiparisi/Form1.cs
+++ CemalElgay_KafeSiparisi/CemalElgay_KafeSiparisi/Form1.cs
@@ -36,6 +36,9 @@ namespace CemalElgay_KafeSiparisi
 
             #region Masa Button
            
+
+
+
             #endregion
         }
     }
