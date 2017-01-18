ddccd4c4b34da056ee3f9343057a0f70f36cf7f9
cemal.elgay <celgay@hotmail.com>
2017-01-18 11:35:53 +0200

    Updated

---
+++ SQL_Test/SQL_Test/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection baglan = new SqlConnection("Server=ALS-YG39104-J12;Database=Northwind;UID=sa;PWD=123456789?");
+
+            //SqlCommand cmd = new SqlCommand("INSERT INTO Categories(CategoryName, Description) VALUES ('Yeni Kategori','Açıklama')",baglan);
+
+            //SqlCommand cmd = new SqlCommand("DELETE FROM Categories WHERE CategoryID = 9", baglan);
+            //baglan.Open();
+
+            //cmd.ExecuteNonQuery();
+
+            //baglan.Close(); // Bağlantıyı kapatır.
+            //baglan.Dispose(); // Hem baglantıyı kapatır hemde RAM da işlemi temizler.
+
+            SqlDataAdapter dap = new SqlDataAdapter("SELECT * FROM Employees", baglan);
+
+            DataTable dt = new DataTable();
+            dap.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
