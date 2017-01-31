bd307dad982ec18895680b46f30160f67c9e108b
cemal.elgay <celgay@hotmail.com>
2017-01-31 12:01:46 +0200

    CemalDB

---
+++ CemalBilgisayar/CemalBilgisayar/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace CemalBilgisayar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnEkle = new System.Windows.Forms.Button();
+            this.TxtUrunIsmi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtUrunFiyat = new System.Windows.Forms.TextBox();
+            this.BtnListele = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnEkle
+            // 
+            this.BtnEkle.Location = new System.Drawing.Point(420, 361);
+            this.BtnEkle.Name = "BtnEkle";
+            this.BtnEkle.Size = new System.Drawing.Size(75, 57);
+            this.BtnEkle.TabIndex = 0;
+            this.BtnEkle.Text = "EKLE";
+            this.BtnEkle.UseVisualStyleBackColor = true;
+            this.BtnEkle.Click += new System.EventHandler(this.BtnEkle_Click);
+            // 
+            // TxtUrunIsmi
+            // 
+            this.TxtUrunIsmi.Location = new System.Drawing.Point(61, 381);
+            this.TxtUrunIsmi.Name = "TxtUrunIsmi";
+            this.TxtUrunIsmi.Size = new System.Drawing.Size(100, 20);
+            this.TxtUrunIsmi.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 361);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ürün İsmi";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 61);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(518, 289);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(313, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ürün Fiyat";
+            // 
+            // TxtUrunFiyat
+            // 
+            this.TxtUrunFiyat.Location = new System.Drawing.Point(268, 381);
+            this.TxtUrunFiyat.Name = "TxtUrunFiyat";
+            this.TxtUrunFiyat.Size = new System.Drawing.Size(100, 20);
+            this.TxtUrunFiyat.TabIndex = 5;
+            // 
+            // BtnListele
+            // 
+            this.BtnListele.Location = new System.Drawing.Point(15, 27);
+            this.BtnListele.Name = "BtnListele";
+            this.BtnListele.Size = new System.Drawing.Size(146, 28);
+            this.BtnListele.TabIndex = 6;
+            this.BtnListele.Text = "Ürünler Listele";
+            this.BtnListele.UseVisualStyleBackColor = true;
+            this.BtnListele.Click += new System.EventHandler(this.BtnListele_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 430);
+            this.Controls.Add(this.BtnListele);
+            this.Controls.Add(this.TxtUrunFiyat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtUrunIsmi);
+            this.Controls.Add(this.BtnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnEkle;
+        private System.Windows.Forms.TextBox TxtUrunIsmi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtUrunFiyat;
+        private System.Windows.Forms.Button BtnListele;
+    }
+}
+
---
+++ CemalBilgisayar/CemalBilgisayar/Form1.cs
@@ -0,0 +1,43 @@
+﻿using CemalBilgisayar.DataBaseConnection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CemalBilgisayar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            using (cemalDBContext con = new cemalDBContext())
+            {
+                var result = con.Urunlers.ToList();
+                dataGridView1.DataSource = result;
+            }
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            using (cemalDBContext con = new cemalDBContext())
+            {
+                Urunler u = new Urunler();
+                u.UrunAdi = TxtUrunIsmi.Text;
+                u.UrunFiyati = Convert.ToDecimal(TxtUrunFiyat.Text);
+
+                con.Urunlers.Add(u);
+                con.SaveChanges();
+            }
+        }
+    }
+}
