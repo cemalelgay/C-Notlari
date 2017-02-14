6a587b1161cb1c9114939e913c0db0e2df5826b3
cemal.elgay <celgay@hotmail.com>
2017-02-14 11:55:31 +0200

    WEB_3

---
+++ JSDevam/JSDevam/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JSDevam.Global" Language="C#" %>
---
+++ JSDevam/JSDevam/default.js
@@ -0,0 +1,23 @@
+﻿var kisiListesi = [];
+var kisiEkle = function ()
+{
+    var kisi = {};
+    kisi.Ad = Get('txtAdSoyad').value;
+    if (kisi.Ad == "")  
+        alert("Uygun formatta isim giriniz")    
+    else   
+        kisi.email = get('txtEmail').value;
+    
+    if (kisi.email == "")   
+        alert("emaili dogru formatta giriniz")
+    kisi.bilgiGetir = function()
+    {
+        return kisi.Ad + " " + kisi.email;
+    }
+    kisiListesi.push(kisi);
+    Get('kisiler').innerHTML += kisi.bilgiGetir() + "</br>";
+}
+function Get(elementID)
+{
+    return document.getElementById(elementID);
+}
