5b5c496cfe591acb3518079dddb566418b0e137b
cemal.elgay <celgay@hotmail.com>
2017-02-20 09:32:48 +0200

    WEB

---
+++ PostBack/PostBack/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PostBack
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DropDownList1.Items.Add("Aytekin");
+                DropDownList1.Items.Add("Çeyiz");
+                DropDownList1.Items.Add("Hakan");
+                DropDownList1.Items.Add("Bengisu");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DropDownList1.Items.Add("Atakan");
+        }
+        //Butona tıklayınca yeniden page_load tekrar ekler
+        //İlk form yüklendiğinde postback true olur.
+    }
+}
