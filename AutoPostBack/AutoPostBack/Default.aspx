06c8f7b54d219d6065b83849cae1fcf85d9618a7
cemal.elgay <celgay@hotmail.com>
2017-02-20 09:56:14 +0200

    WEB

---
+++ AutoPostBack/AutoPostBack/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AutoPostBack.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Lütfen İl Seçin"></asp:Label>
+    
+    </div>
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+        </asp:DropDownList>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Lütfen İlçe Seçin"></asp:Label>
+        </p>
+        <asp:DropDownList ID="DropDownList2" runat="server">
+        </asp:DropDownList>
+    </form>
+</body>
+</html>
---
+++ AutoPostBack/AutoPostBack/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AutoPostBack
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DropDownList1.Items.Add("İl Seçiniz...");
+                DropDownList1.Items.Add("İzmir");
+                DropDownList1.Items.Add("İstanbul");
+                DropDownList1.Items.Add("Ankara");
+
+                DropDownList2.Items.Add("İlçe Seçiniz...");
+                DropDownList2.Enabled = false;
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DropDownList2.Enabled = true;
+
+            if (DropDownList1.SelectedIndex==1)
+            {
+                DropDownList2.Items.Clear();
+                DropDownList2.Items.Add("Karşıyaka");
+                DropDownList2.Items.Add("Konak");
+                DropDownList2.Items.Add("Buca");
+            }
+            else if(DropDownList1.SelectedIndex==2)
+            {
+                DropDownList2.Items.Clear();
+                DropDownList2.Items.Add("Aksaray");
+                DropDownList2.Items.Add("Kadıköy");
+                DropDownList2.Items.Add("Eminönü");
+            }
+            else if(DropDownList1.SelectedIndex==3)
+            {
+                DropDownList2.Items.Clear();
+                DropDownList2.Items.Add("Dikmen");
+                DropDownList2.Items.Add("Keçiören");
+                DropDownList2.Items.Add("Polatlı");
+            }
+        }
+    }
+}
